6188b93e53453c0b119479c01a4b2cf387260b8d
Marlon Ou <omzmarlon@gmail.com>
2017-10-24 15:54:10 -0700

    Junit example

---
+++ src/test/java/com/redcross/test/DemoTest.java
@@ -0,0 +1,12 @@
+package com.redcross.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
+}
